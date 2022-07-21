98a37767d1286fb2fee4ecb93638dd5c6fc0fd2a
omaran bazna <omran4d@gmail.com>
2022-07-21 14:46:09 -0400

    writing the steps

--- Mathmatica.js
+++ Mathmatica.js
@@ -6,6 +6,11 @@
 ///figuring out the pade series  for the first An, Bn
 ///pade Series at order of n =(A0+A1*x^1+A2*x^2+A3*x^3...+An*x^n)/(B0+B1*x^1+B2*x^2+B3*x^3...+Bn*x^n) =tylor series at order of n=segma(0,n) (-1)^n*x^(2*n+1)/(2*n+1)!
 
+///we need to figure out the An from n=0 to n=100;
+
+///we will create map 'mapAn'  of (x^i) for the left right of the equation
+///and a map 'mapBn' for of  (x^i) for the right side of the equation
+///
 class MathMatica {
   sin(x) {
     ///I will use pade series
