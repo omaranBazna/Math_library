4d8bc1a5838a1178c69e70be4ed4430b116928d1
omaran bazna <omran4d@gmail.com>
2022-07-21 14:43:51 -0400

    commenting the formula for pade series

--- Mathmatica.js
+++ Mathmatica.js
@@ -3,8 +3,11 @@
 ///we will use Pade series instead of tylor series
 ///https://www.youtube.com/watch?v=szMaPkJEMrw
 
+///figuring out the pade series  for the first An, Bn
+///pade Series at order of n =(A0+A1*x^1+A2*x^2+A3*x^3...+An*x^n)/(B0+B1*x^1+B2*x^2+B3*x^3...+Bn*x^n) =tylor series at order of n=segma(0,n) (-1)^n*x^(2*n+1)/(2*n+1)!
+
 class MathMatica {
   sin(x) {
-    ///I will use paddes
+    ///I will use pade series
   }
 }
