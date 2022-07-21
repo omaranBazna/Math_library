e9ef40adf79167f856907e3065362ac19b03435f
omaran bazna <omran4d@gmail.com>
2022-07-21 14:32:35 -0400

    commenting the references

--- Mathmatica.js
+++ Mathmatica.js
@@ -1,3 +1,10 @@
+///Omaran Bazna
+///7/21/2022
+///we will use Pade series instead of tylor series
+///https://www.youtube.com/watch?v=szMaPkJEMrw
+
 class MathMatica {
-  sin(x) {}
+  sin(x) {
+    ///I will use paddes
+  }
 }
